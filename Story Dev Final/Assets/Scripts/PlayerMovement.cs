e6f71fb5d05f62b4bd06cca767293b76e01b7074
Ryan Staudacher <slyry7146@gmail.com>
2017-05-02 16:42:36 -0500

    Added placeholder stuff

---
+++ Story Dev Final/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+	}
+}
